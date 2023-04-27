5cd791174a004d2c81db84ece2947256922e86db
renginalkanfzs <rengin.alkan@fzsbank.com.tr>
2023-04-27 16:37:56 +0300

    ProfileCardInfo tamamlandı.

--- src/Components/ProfileİnfoCard.js
+++ src/Components/ProfileİnfoCard.js
@@ -2,29 +2,30 @@ import { useEffect, useState } from 'react';
 import React from 'react';
 import styles from './styles.module.css'
 import './Profile.css'
+import{FaCertificate}from 'react-icons/fa';
 
 export default function ProfileCardInfo(){
     return(
         <div className='profile-info'>
-        <div>FRONTEND LİMİTED</div>
+        <div className='company-name'><FaCertificate/>FRONTEND LİMİTED</div>
         <div className='infos'>
-            <div>
+            <div className='info'>
                 <span>
-                    <p>Web Site</p>
+                    <p className='title'>Web Site</p>
                     <p>https://envisionfintech.com</p>
                 </span>
-                <span>
-                <p> Kayıt Tarihi</p>
-                <p>https://envisionfintech.com</p>  
+                <span className='element'>
+                <p className='title '> Kayıt Tarihi</p>
+                <p>12 Mar 2023</p>  
                 </span>
             </div>
             <div>
-                <span>İş Tipi</span>
-                <span>Tamamen Çevrimiçi e-Ticaret / D2C</span>
+                <span className='title'>İş Tipi</span>
+                <p>Tamamen Çevrimiçi e-Ticaret / D2C</p>
             </div>
             <div>
-                <span>Ülke</span>
-                <span>Türkiye</span>
+                <span className='title'>Ülke</span>
+                <p>Türkiye</p>
             </div>
         </div>
     </div>
