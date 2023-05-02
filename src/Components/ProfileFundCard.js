270d6d929bf4d69e1412c06619eb0b6ca72d4096
renginalkanfzs <rengin.alkan@fzsbank.com.tr>
2023-05-02 09:01:49 +0300

    ProfileFundCard ve Profile.css dosyalarında revizeler için değişiklik yapıldı.

--- src/Components/ProfileFundCard.js
+++ src/Components/ProfileFundCard.js
@@ -21,17 +21,42 @@ export default function ProfileFundCard(){
                  <Button className="fund-button" label="Devam Et" />
                  </div>
              </div>
-             <div className='fund-info'>
-                <div className='info'>
-                     <span>
-                         <p className='title'>Bekleyen Yatırım İncelemesi</p>
-                         <p>Başvurunuz inceleniyor. Bir ön koşul sayfası almak için lütfen gelir doğrulamaları için daha fazla ticari ve finansal bilgi sağlamaya devam edin.</p>
-                     </span> 
-                
-                </div>
-                 <div style={{borderLeft:"solid 1px #000000"}}>
-                 <span></span>
+             <div className='investment'>
+                    <div className='teklif'>
+                        <div className='info'>
+                            <span className='company-name'>
+                                 Teklif Detaylarım
+                             </span> 
+                            <div style={{display:"flex",justifyContent:"space-between", width:"500px"}}>
+                                <span className='element'>
+                                     <p>Fon Tutarı</p>
+                                     <p>USD 0</p>
+                                 </span> 
+                                 <span className='element'>
+                                     <p>Toplam Havale Tutarı</p>
+                                 </span> 
+                                 <span className='element'>
+                                     <p>Havale Oranı</p>
+                                 </span>
+                            </div>
+                        </div>
+                         <div style={{borderLeft:"solid 1px rgb(218 218 218)",paddingLeft:"20px", marginLeft:"100px"}}>
+                             <span className='company-name'>
+                             Talep Ettiğim Miktar
+                            </span> 
+                             <div className='teklif'>
+                                 <span className='element'>
+                                     <p>Talep Edilen Fon Tutarı</p>
+                                     <p>USD 15,000.00</p>
+                            </span> 
+                        </div>
+                 
                  </div>
+                     </div>
+                     
+                
+                
+              
              </div>
         
         </div>
