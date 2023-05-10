d85882ca9341e355a8131c185796a648d74d5855
renginalkanfzs <rengin.alkan@fzsbank.com.tr>
2023-05-10 15:38:49 +0300

    Modal için gerekli olan bootsrap paketi eklendi

--- src/Components/BankAccountsTabCard.js
+++ src/Components/BankAccountsTabCard.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import{FaCertificate}from 'react-icons/fa';
 import  './Setting.css'
 import styles from './styles.module.css'
 import Button from './button';
+import AddBankAccount from './modals/AddBankAccount'
+
 
 export default function BankAccountsTabCard() {
+    const [modalOpened, setModalOpened] = useState(false);
     return(
         <div className='accounts-bank'>
             <h4>Bize Finansal Bilgilerinizi Verin</h4>
             <h6>Mali bilgileriniz, finansman uygunluğunuzu belirleyecektir. Banka hesaplarınızı ve ekstrelerinizi eklemek, mümkün olan en fazla sermayeyi almanızı sağlayacaktır.</h6>
             <h5>Banka Hesapları</h5>
             <div className='bank'></div>
-            <Button className="fund-button" label="Banka Hesabı Ekle" />
+
+            <Button className="fund-button" onClick={() => setModalOpened(true)} label="Banka Hesabı Ekle" />
+            
+            {modalOpened && <AddBankAccount  setOpenModal={setModalOpened}/>}
         </div>
     )}
